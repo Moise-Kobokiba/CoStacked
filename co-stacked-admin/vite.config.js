35a5126ed95323727c72b1661e5af0feac02d560
Super User <root@webserver1.costacked.co.za>
2025-12-15 21:10:31 +0200

    Add www subdomain to Vite allowed hosts

--- co-stacked-admin/vite.config.js
+++ co-stacked-admin/vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: [
+      'admin.costacked.co.za',
+      'www.admin.costacked.co.za',
+      'localhost',
+      '127.0.0.1',
+      '154.66.197.173'
+    ]
+  }
 })
