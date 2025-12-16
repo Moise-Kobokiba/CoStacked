9d02f9a8c3ecffb32c02d15bf9e73c744126ee00
Momo <clement03jr@gmail.com>
2025-12-16 10:49:56 +0200

    Update vite.config.js

--- co-stacked-frontend/vite.config.js
+++ co-stacked-frontend/vite.config.js
@@ -1,16 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: true, // <-- allows connections from any host
     allowedHosts: [
       'costacked.co.za',
       'www.costacked.co.za',
       'localhost',
       '127.0.0.1',
       '154.66.197.173'
-    ]
-  }
-})
+    ],
+  },
+});
