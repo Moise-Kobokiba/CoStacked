2975b6daf3c52d3ed4669909d80c09b8da335481
Momo <clement03jr@gmail.com>
2025-12-16 11:04:41 +0200

    Update vite.config.js

--- co-stacked-admin/vite.config.js
+++ co-stacked-admin/vite.config.js
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  server: {
+  server: mode === 'development' ? {
+    host: true,
     allowedHosts: [
       'admin.costacked.co.za',
       'www.admin.costacked.co.za',
@@ -12,5 +12,5 @@ export default defineConfig({
       '127.0.0.1',
       '154.66.197.173'
     ]
-  }
-})
+  } : undefined
+}));
